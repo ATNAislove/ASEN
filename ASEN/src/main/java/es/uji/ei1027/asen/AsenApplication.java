e0ba5e2159cbdbafec008451d7c340a994d5c22b
ATNAislove <al375915@uji.es>
2021-03-30 13:41:06 +0200

    First commit

---
+++ ASEN/src/main/java/es/uji/ei1027/asen/AsenApplication.java
@@ -0,0 +1,37 @@
+package es.uji.ei1027.asen;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+@SpringBootApplication
+public class AsenApplication implements CommandLineRunner {
+
+    private static final Logger log = Logger.getLogger(AsenApplication .class.getName());
+
+    public static void main(String[] args) {
+        // Auto-configura l'aplicació
+        new SpringApplicationBuilder(AsenApplication .class).run(args);
+    }
+
+    // Funció principal
+    public void run(String... strings) throws Exception {
+        log.info("Ací va el meu codi");
+    }
+
+    // Configura l'accés a la base de dades (DataSource)
+    // a partir de les propietats a src/main/resources/applications.properties
+    // que comencen pel prefix spring.datasource
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
