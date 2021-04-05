26850c19ee96927068cca85d1e86cdf56014d2a2
ATNAislove <al375915@uji.es>
2021-04-05 17:49:20 +0200

    gestorMunicipal modelo creado

--- ASEN/src/main/java/es/uji/ei1027/asen/model/GestorMunicipal.java
+++ ASEN/src/main/java/es/uji/ei1027/asen/model/GestorMunicipal.java
@@ -1,4 +1,125 @@
 package es.uji.ei1027.asen.model;
 
+import java.time.LocalDate;
+
 public class GestorMunicipal {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+    private String correoElectronico;
+    private String direccion;
+    private String municipioNacimiento;
+    private String pais;
+    private long telefono;
+    private LocalDate fechaRegistro;
+    private LocalDate fechaBaja;
+
+    public GestorMunicipal() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getMunicipioNacimiento() {
+        return municipioNacimiento;
+    }
+
+    public void setMunicipioNacimiento(String municipioNacimiento) {
+        this.municipioNacimiento = municipioNacimiento;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public LocalDate getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(LocalDate fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+
+    @Override
+    public String toString() {
+        return "GestorMunicipal{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", correoElectronico='" + correoElectronico + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", municipioNacimiento='" + municipioNacimiento + '\'' +
+                ", pais='" + pais + '\'' +
+                ", telefono=" + telefono +
+                ", fechaRegistro=" + fechaRegistro +
+                ", fechaBaja=" + fechaBaja +
+                '}';
+    }
 }
