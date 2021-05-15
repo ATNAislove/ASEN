2e4bb089a3965802b36e932aa36aea1c1c27dfe1
VictorinoPlatino <al375889@uji.es>
2021-05-15 19:06:07 +0200

    Revisado error de TipoServicio.

--- ASEN/src/main/java/es/uji/ei1027/asen/model/tipoServicio.java
+++ ASEN/src/main/java/es/uji/ei1027/asen/model/tipoServicio.java
@@ -1,15 +1,30 @@
 package es.uji.ei1027.asen.model;
 
-public class tipoServicio {
-    private int idServicio;
+public class TipoServicio {
+    private int idTipoServicio;
     private String nombre;
 
-    public int getIdServicio() {
-        return idServicio;
+    public int getIdTipoServicio() {
+        return idTipoServicio;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public void setIdTipoServicio(int idTipoServicio) {
+        this.idTipoServicio = idTipoServicio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "tipoServicio{" +
+                "idTipoServicio=" + idTipoServicio +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
